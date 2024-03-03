d89e431e39597716bcf98d3f2f220cf85790becb
Naitik Agrawal <151402099+naitik-2006@users.noreply.github.com>
2024-03-03 21:39:38 +0530

    feature added

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
 import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-// piece list order
+// piece lsit order
 // king quees rook bishop knight pawn
 
 public class App {
@@ -16,17 +17,46 @@ public class App {
 		Display gameDisplay = new Display(game);
 		JFrame gameWindow = new JFrame("Chess");
 
-        ImageIcon logoImage = new ImageIcon("lib/images/app_icon.png");
+        ImageIcon logoImage = new ImageIcon("src/images/app_icon.png");
         gameWindow.setIconImage(logoImage.getImage());
         gameWindow.getContentPane().setBackground(new Color(155,155,200));
 		
-		gameWindow.setResizable(false);
 		gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameWindow.add(gameDisplay);
         gameWindow.pack();
 		gameWindow.setVisible(true);
 		gameWindow.setLocationRelativeTo(null);
+		askTime();
 
+		
 		new MouseInput(gameDisplay);
 	}
+	static public void askTime() {
+
+		String[] promote = {"5", "10", "15"};
+
+		JFrame frame = new JFrame();
+		frame.setResizable(true);	
+
+		int n = JOptionPane.showOptionDialog(frame,
+				"What do you want to promote to?",
+				"Choose a piece:",
+				JOptionPane.DEFAULT_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, promote, promote[0]);
+		
+		while(true) {
+			if(n == JOptionPane.CLOSED_OPTION) {
+				n = JOptionPane.showOptionDialog(frame,
+						"What do you want to promote to?",
+						"Choose a piece:",
+						JOptionPane.DEFAULT_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null, promote, promote[0]);
+				continue;
+			}
+			break;
+		}
+	}
+
 }
