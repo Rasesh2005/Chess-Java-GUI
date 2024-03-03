59c87de5fd3ac40794213b34634701f30e64fc70
Rasesh Shetty <rasesh.udayshetty@gmail.com>
2024-03-03 11:28:04 +0530

    Added chessboard operations

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -23,4 +23,56 @@ public class ChessBoard {
 		
 		return previousMoves.peek();
 	}
+	public void move(Piece piece, BoardCoordinate tile) {
+		int sourceX = piece.getCoordinate().getX();
+		int sourceY = piece.getCoordinate().getY();
+		int destX = tile.getX();
+		int destY = tile.getY();
+		int direction = 0; 
+		Piece tilePiece = getPiece(destX, destY);
+		
+		if(tilePiece != null) {
+			tilePiece.getOwner().addToGraveyard(tilePiece);
+		}
+		this.previousMoves.push(new MoveHistory(piece,new BoardCoordinate(sourceX, sourceY)));
+		if(piece instanceof Pawn) {
+			((Pawn)piece).setFirstMove(false);
+			String color = piece.getColor();
+			if((destX != sourceX) && tilePiece == null) {// en passant case
+				if(color.equals("White")) {
+					// direction = Validator.UP;
+					// UP DOWN LEFT RIGHT are static variable stating direction
+				}
+				else {
+					// direction = Validator.DOWN;
+				}
+				
+				tilePiece = getPiece(destX, destY-direction);
+				tilePiece.getOwner().addToGraveyard(tilePiece);
+				board[destX][destY-direction] = null;
+			}
+		}else if(piece instanceof King) {
+			((King)piece).setHasMoved(true);
+			
+			if(destX == sourceX + 2 ) {// short castling
+				Piece rightRook = board[destX+1][destY];
+				board[destX - 1][destY] = rightRook;
+				rightRook.setCoordinate(new BoardCoordinate(destX-1,destY));
+				
+				board[destX + 1][destY] = null;
+			}
+			else if(destX == sourceX - 2) {// long castling
+				Piece leftRook = board[destX-2][destY];
+				board[destX + 1][destY] = leftRook;
+				leftRook.setCoordinate(new BoardCoordinate(destX+1,destY));
+
+				board[destX - 2][destY] = null;
+			}
+		}else if(piece instanceof Rook) {
+			((Rook)piece).setHasMoved(true);
+		}
+		board[sourceX][sourceY] = null;
+		board[destX][destY] = piece;
+		piece.setCoordinate(tile);
+	}
 }
