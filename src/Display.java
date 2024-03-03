4fdf20555c80639a295f0e286a2cb8879ffb21d4
Naitik Agrawal <151402099+naitik-2006@users.noreply.github.com>
2024-03-03 15:09:27 +0530

    add piece to display

--- src/Display.java
+++ src/Display.java
@@ -1,28 +1,46 @@
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Color;
 import java.awt.Dimension;
 
+// images 0 - white, 1- black
+// peices order king, queen, rook, bishop, knight, pawn
+
 public class Display extends JPanel {
 
-    public static final int TILE_SIZE = 80;
+    public static final int TILE_SIZE = 100;
 	private static final Color GREEN = new Color(0x96, 0xFF, 0x96);
 	private static final Color LIGHT_BLUE = new Color(0xC8, 0xE6, 0xFF);
 	private static final Color DARK_BLUE = new Color(0x7D, 0xC8, 0xFF);
 	private static final Color LIGHT_RED = new Color(0xFF, 0x50, 0x50);
 	private static final Color DARK_RED = new Color(0xC8, 0x00, 0x00);
-	private static final Color LIGHT_TILE = new Color(210, 165, 125);
-	private static final Color DARK_TILE = new Color(175, 115, 70);
+	private static final Color LIGHT_TILE = new Color(235, 236, 208);
+	private static final Color DARK_TILE = new Color(119, 149, 86);
     private Graphics2D g2d;
     private Game game;
     private ChessBoard chessboard;
+	private Image images[][] = new Image[2][6];
     
     public Display(Game game){
         this.setPreferredSize(new Dimension(TILE_SIZE*8, TILE_SIZE*8));
         this.game = game;
 		this.chessboard = game.getChessBoard();
+		images[0][0] = getPieceImage("king", "white");
+		images[0][1] = getPieceImage("queen", "white");
+		images[0][2] = getPieceImage("rook", "white");
+		images[0][3] = getPieceImage("bishop", "white");
+		images[0][4] = getPieceImage("knight", "white");
+		images[0][5] = getPieceImage("pawn", "white");
+		images[1][0] = getPieceImage("king", "black");
+		images[1][1] = getPieceImage("queen", "black");
+		images[1][2] = getPieceImage("rook", "black");
+		images[1][3] = getPieceImage("bishop", "black");
+		images[1][4] = getPieceImage("knight", "black");
+		images[1][5] = getPieceImage("pawn", "black");
     }
 
     @Override
@@ -61,8 +79,43 @@ public class Display extends JPanel {
 	    }
 	}
 
-    private void drawPiece(Piece piece, int i, int j) {
-        
+    private void drawPiece(Piece piece, int x, int y) {
+        if(piece.getColor().equals("WHITE")) {
+			if(Pawn.class.isInstance(piece))
+				g2d.drawImage(images[0][5], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(Rook.class.isInstance(piece))
+				g2d.drawImage(images[0][2], TILE_SIZE * x, TILE_SIZE * y, null);		
+			else if(Knight.class.isInstance(piece))
+				g2d.drawImage(images[0][4], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(Bishop.class.isInstance(piece))
+				g2d.drawImage(images[0][3], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(Queen.class.isInstance(piece))
+				g2d.drawImage(images[0][1], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(King.class.isInstance(piece))
+				g2d.drawImage(images[0][0], TILE_SIZE * x, TILE_SIZE * y, null);	
+		}
+		else {
+			if(Pawn.class.isInstance(piece))
+				g2d.drawImage(images[1][5], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(Rook.class.isInstance(piece))
+				g2d.drawImage(images[1][2], TILE_SIZE * x, TILE_SIZE * y, null);		
+			else if(Knight.class.isInstance(piece))
+				g2d.drawImage(images[1][4], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(Bishop.class.isInstance(piece))
+				g2d.drawImage(images[1][3], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(Queen.class.isInstance(piece))
+				g2d.drawImage(images[1][1], TILE_SIZE * x, TILE_SIZE * y, null);	
+			else if(King.class.isInstance(piece))
+				g2d.drawImage(images[1][0], TILE_SIZE * x, TILE_SIZE * y, null);	
+		}
     }
+
+	private Image getPieceImage(String piece, String color){
+		ImageIcon icon = new ImageIcon(getClass().getResource("../bin//images/" + color + "_" + piece + ".png"));
+		Image image = icon.getImage().getScaledInstance(TILE_SIZE, TILE_SIZE, Image.SCALE_SMOOTH);
+		icon = new ImageIcon(image, icon.getDescription());
+		// System.out.println(getClass());
+		return icon.getImage();
+	}
 
 }
