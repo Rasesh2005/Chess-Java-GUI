58ad981c918d701880b54f053c153dca9e689ce1
Rasesh Shetty <rasesh.udayshetty@gmail.com>
2024-03-03 11:31:36 +0530

    added piece to chessboard

--- src/Player.java
+++ src/Player.java
@@ -7,15 +7,25 @@ public class Player {
     private List<Piece> pieceList;
     private Stack<Piece> graveyard;
     private boolean underCheck;
-    public void initializePieces(){
-        
-    }
+    private void initializePieces() {
+		pieceList.add(new King(this, color));
+		pieceList.add(new Queen(this, color));
+		for(int i = 0; i < 2; i++) {
+			pieceList.add(new Rook(this, color));
+			pieceList.add(new Bishop(this, color));
+			pieceList.add(new Knight(this, color));
+		}
+		for(int i = 0; i < 8; i++) {
+			pieceList.add(new Pawn(this, color));
+		}
+	}
     public void initialize(String color) {
 		pieceList = new ArrayList<Piece>();
 		graveyard = new Stack<Piece>();
 		this.color = color;
 		this.initializePieces();
 	}
+	
     public String getColor() {
 		return color;
 	}
